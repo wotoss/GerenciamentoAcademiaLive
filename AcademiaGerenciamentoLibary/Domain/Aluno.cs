460f015c6bd1253e20932606a372fa453c826fdd
WOTO <wotoss10@gamil.com>
2025-04-03 17:18:56 -0300

    modelando classes Aluno e Pagamento

--- AcademiaGerenciamentoLibary/Domain/Aluno.cs
+++ AcademiaGerenciamentoLibary/Domain/Aluno.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AcademiaGerenciamentoLibary.Domain
 {
+    [Table("Aluno")]
     public class Aluno
     {
+        [Key]
+        public int ID { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Whatsapp { get; set; } = string.Empty;
+        [Required]
+        [StringLength(11)]
+        public string Cpf { get; set; } = string.Empty;
+        public DateTime? DataNascimento { get; set; }
+        [Required]
+        public byte Status { get; set; } = 1;
+        public DateTime? DataPagamento { get; set; } = DateTime.Now;
     }
 }
---
+++ AcademiaGerenciamentoLibary/Domain/Pagamento.cs
@@ -0,0 +1,22 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace AcademiaGerenciamentoLibary.Domain
+{
+    public class Pagamento
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public int AlunoID { get; set; }
+        public DateTime? DataPagamento { get; set; }
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal ValorPago { get; set; }
+        [StringLength(50)]
+        public string MetodoPagamento { get; set; } = string.Empty;    
+        public byte StatusPagamento { get; set; }
+    }
+}
