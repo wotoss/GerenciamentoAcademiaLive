ff447bd556a6a46afbc072cd798e6c3e75490988
WOTO <wotoss10@gamil.com>
2025-04-03 11:58:44 -0300

    adicionando classe Aluno

---
+++ AcademiaGerenciamentoLibary/Domain/Aluno.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademiaGerenciamentoLibary.Domain
+{
+    public class Aluno
+    {
+    }
+}
